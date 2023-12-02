d31ff912c23e4989c985ca98b89631318dd88927
nor <nor@ik.me>
2023-12-02 09:50:18 +0100

    Part 2

--- day02.py
+++ day02.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 with open("day2.txt", "r") as of:
     data = of.read().strip()
 
@@ -8,7 +10,6 @@ class Game:
 
     def __init__(self, hist):
         prefix, sets = hist.split(": ")
-        self.sets_ = sets
         self.id = int(prefix.split(" ")[-1])
         self.sets = self.parse_game(sets)
 
@@ -33,7 +34,18 @@ class Game:
         )
         return possible
 
+    @property
+    def minimum_requirements(self):
+        return [max([el[i] for el in self.sets]) for i in range(3)]
+
+    @property
+    def power(self):
+        return reduce(lambda x, y: x * y, self.minimum_requirements)
+
 
 # Part one
 games = [Game(h) for h in data.split("\n")]
 print(sum([g.id for g in games if g.is_possible]))
+
+# Part two
+print(sum([g.power for g in games]))
