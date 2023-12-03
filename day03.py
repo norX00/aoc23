39700f8ab7eefe83f9aeabd4654be430dbb329db
nor <nor@ik.me>
2023-12-03 10:37:42 +0100

    Day 3 part two

--- day03.py
+++ day03.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 with open("day3.txt", "r") as of:
     data = of.read().strip()
 
@@ -19,8 +21,28 @@ for idx, line in enumerate(data.split("\n")):
 
 
 def adjacent(nums_pos, sym_pos):
-    return any([max(abs(num_pos[0] - sym_pos[0]), abs(num_pos[1] - sym_pos[1])) <= 1for num_pos in nums_pos])
+    return any(
+        [
+            max(abs(num_pos[0] - sym_pos[0]), abs(num_pos[1] - sym_pos[1])) <= 1
+            for num_pos in nums_pos
+        ]
+    )
 
 
 # Part one
 print(sum([num for coords, num in numbers.items() if any([adjacent(coords, coord) for coord in symbols.keys()])]))
+
+
+# Part two
+def gear_ratio(num_list):
+    return reduce(lambda x, y: x * y, num_list) * int(len(num_list) == 2)
+
+
+print(
+    sum(
+        [
+            gear_ratio([num for coords, num in numbers.items() if any([adjacent(coords, gear_coord)])])
+            for gear_coord in [coord for coord, sym in symbols.items() if sym == "*"]
+        ]
+    )
+)
