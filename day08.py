03d1ba1a3e9cfab3f8066c06112145ce6bc96964
nor <nor@ik.me>
2023-12-08 18:16:49 +0100

    Day 8 part two

--- day08.py
+++ day08.py
@@ -1,4 +1,5 @@
 from itertools import cycle
+from math import lcm
 
 with open("day8.txt", "r") as of:
     data = of.read().strip()
@@ -20,3 +21,15 @@ for step, instruction in enumerate(instructions(), 1):
     if current == "ZZZ":
         print(step)
         break
+
+
+# Part two
+least_common_multiple = 1
+for entrance in filter(lambda x: x[-1] == "A", nodes.keys()):
+    current = entrance
+    for step, instruction in enumerate(instructions(), 1):
+        current = nodes.get(current)[instruction]
+        if current[-1] == "Z":
+            least_common_multiple = lcm(least_common_multiple, step)
+            break
+print(least_common_multiple)
