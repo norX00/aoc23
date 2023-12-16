c2a84911c0502f50859f6b88e8a33d324a0a7ec7
Nor Jaafari <nor@ik.me>
2023-12-16 15:05:11 +0100

    Day 15 part two

--- day15.py
+++ day15.py
@@ -3,5 +3,18 @@ from functools import reduce
 with open("day15.txt", "r") as of:
     data = of.read().strip()
 
+
+def get_hash(current_value, char):
+    return (17*(current_value+ord(char))) % 256
+
+
 # Part one
-print(sum(map(lambda z: reduce(lambda x, y: (17 * (x + ord(y))) % 256, z, 0), data.split(","))))
+print(sum(map(lambda z: reduce(lambda x, y: get_hash(x, y), z, 0), data.split(","))))
+
+# Part two - credits reddit/u/4HbQ match statement
+boxes = [dict() for _ in range(256)]
+for instruction in data.split(","):
+    match instruction.strip("-").split("="):
+        case [lens, focal]: boxes[reduce(get_hash, lens, 0)][lens] = int(focal)
+        case [lens]: boxes[reduce(get_hash, lens, 0)].pop(lens, 0)
+print(sum(box_idx * slot * focal for box_idx, box in enumerate(boxes, 1) for slot, focal in enumerate(box.values(), 1)))
