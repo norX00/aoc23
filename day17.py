1c49eb87e484a3b164668603db40d39a6f68995b
Nor Jaafari <nor@ik.me>
2023-12-17 11:54:17 +0100

    Day 17 part two

--- day17.py
+++ day17.py
@@ -10,11 +10,11 @@ def opposite(d):
 
 grid = data.split("\n")
 directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-bottom_right = (len(grid[0])-1, len(grid)-1)
+bottom_right = (len(grid[0]) - 1, len(grid) - 1)
 heat_map = {(x, y): int(c) for y, row in enumerate(grid) for x, c in enumerate(row)}
 
 
-def shortest_path():
+def shortest_path(min_move=1, max_move=3):
     heat, current, direction = 0, (0, 0), (0, 0)
     heap, passed = [(heat, current, direction)], set()
     while heap:
@@ -25,12 +25,16 @@ def shortest_path():
             passed.add((current, direction))
             for direction in [d for d in directions if d not in [direction, opposite(direction)]]:
                 p, h = current, heat
-                for i in range(1, 4):
+                for steps in range(1, max_move + 1):
                     p = p[0] + direction[0], p[1] + direction[1]
                     if p in heat_map:
                         h += heat_map[p]
-                        heapq.heappush(heap, (h, p, direction))
+                        if steps >= min_move:
+                            heapq.heappush(heap, (h, p, direction))
 
 
 # Part one
 print(shortest_path())
+
+# Part two
+print(shortest_path(4, 10))
