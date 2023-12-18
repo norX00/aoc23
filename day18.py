6a56109bfa8c7e4d50413958de4ba97894724388
Nor Jaafari <nor@ik.me>
2023-12-18 21:01:15 +0100

    Day 18 simplify

--- day18.py
+++ day18.py
@@ -12,30 +12,22 @@ def picks_formula(inner, boundary):
     return int(inner + boundary//2 + 1)
 
 
-current, visited, edge_length = (0, 0), [], 0
-for instruction, steps, _ in [row.split(" ") for row in data.split("\n")]:
-    edge_length += int(steps)
-    for _ in range(int(steps)):
-        visited.append(current)
+def get_polygon_area(instructions):
+    current, corners, edge_length = (0, 0), [], 0
+    for instruction, steps in instructions:
+        edge_length += steps
+        corners.append(current)
         match instruction:
-            case "R": current = (current[0]+1, current[1])
-            case "L": current = (current[0]-1, current[1])
-            case "D": current = (current[0], current[1]+1)
-            case "U": current = (current[0], current[1]-1)
+            case "R" | "0": current = (current[0] + int(steps), current[1])
+            case "L" | "2": current = (current[0] - int(steps), current[1])
+            case "D" | "1": current = (current[0], current[1] + int(steps))
+            case "U" | "3": current = (current[0], current[1] - int(steps))
+    return picks_formula(gauss_area_formula(corners), edge_length)
 
 
 # Part one
-print(picks_formula(gauss_area_formula(visited), edge_length))
+dig_plan = [row.split(" ") for row in data.split("\n")]
+print(get_polygon_area((row[0], int(row[1])) for row in dig_plan))
 
 # Part two
-current, corners, edge_length = (0, 0), [], 0
-for _, _, code in [row.split(" ") for row in data.split("\n")]:
-    instruction, steps = [int(code[-2]), int(code[2:-2], 16)]
-    edge_length += int(steps)
-    corners.append(current)
-    match instruction:
-        case 0: current = (current[0]+int(steps), current[1])
-        case 2: current = (current[0]-int(steps), current[1])
-        case 1: current = (current[0], current[1]+int(steps))
-        case 3: current = (current[0], current[1]-int(steps))
-print(picks_formula(gauss_area_formula(corners), edge_length))
+print(get_polygon_area((row[2][-2], int(row[2][2:-2], 16)) for row in dig_plan))
